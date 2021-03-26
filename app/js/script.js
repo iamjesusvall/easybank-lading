016f394db418c67d8f7551c25fbd8b106a273739
Jesus Valladares <jesusvall17@gmail.com>
2021-03-26 14:19:49 -0400

    Animations hamburger menu added.

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello Easybank!");
+const headerMenuBtn = document.querySelector("#header-menu");
+const header = document.querySelector("#header");
+
+headerMenuBtn.addEventListener("click", (e) => {
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+  } else {
+    header.classList.add("open");
+  }
+});
