ff9c11ba5f31bb940f5c069efeea5da06865e12d
Jesus Valladares <jesusvall17@gmail.com>
2021-04-12 00:21:00 -0400

    Overflow fixed on mobile menu.

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 const headerMenuBtn = document.querySelector("#header-toggle");
 const body = document.querySelector("body");
+const html = document.querySelector("html");
 const header = document.querySelector("#header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
@@ -7,6 +8,7 @@ const fadeElems = document.querySelectorAll(".has-fade");
 headerMenuBtn.addEventListener("click", (e) => {
   if (header.classList.contains("open")) {
     body.classList.remove("noscroll");
+    html.classList.remove("noscroll");
     header.classList.remove("open");
     fadeElems.forEach((element) => {
       element.classList.remove("fade-in");
@@ -14,6 +16,7 @@ headerMenuBtn.addEventListener("click", (e) => {
     });
   } else {
     body.classList.add("noscroll");
+    html.classList.add("noscroll");
     header.classList.add("open");
     fadeElems.forEach((element) => {
       element.classList.remove("fade-out");
